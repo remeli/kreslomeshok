fc2434f0973cd2c8546f5f483d89376df3c0a344
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-22 14:03:33 +0600

    add to_param to catalog.rb

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,5 +2,9 @@ class Catalog < ActiveRecord::Base
   validates :title, :presence => { :message => "поле 'Название' обязательное"}
   validates :permalink, :presence => { :message => "поле 'Прямая ссылка' обязательное"}
   validates :proce, :presence => { :message => "поле 'Цена' обязательное" }
-
+  
+  
+  def to_param
+    permalink
+  end
 end
