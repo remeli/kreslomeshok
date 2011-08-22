a446e06f8823af920483577e04b26dbb32e835e1
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-22 14:02:43 +0600

    add validates to catalog

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,2 +1,6 @@
 class Catalog < ActiveRecord::Base
+  validates :title, :presence => { :message => "поле 'Название' обязательное"}
+  validates :permalink, :presence => { :message => "поле 'Прямая ссылка' обязательное"}
+  validates :proce, :presence => { :message => "поле 'Цена' обязательное" }
+
 end
