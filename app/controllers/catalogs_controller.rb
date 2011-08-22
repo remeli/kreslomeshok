200d42838999849c69feac9d432d659a986b7fb0
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-22 11:57:28 +0600

    fill catalogs controller

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,2 +1,45 @@
 class CatalogsController < ApplicationController
+  def index
+    @catalogs = Catalog.all
+    @title = "Каталог"
+  end
+  
+  def show
+    @catalog = Catalog.find(params[:id])
+    @title = @catalog.name
+  end
+  
+  def new
+    @catalog = Catalog.new
+    @title = "Новый продукт"
+  end
+  
+  def create
+    @catalog = Catalog.new(params[:catalog])
+    if @catalog.save
+      redirect_to(@catalog, :notice => "Продукт успешно добавлен")
+    else
+      render(:action => 'new')
+    end
+  end
+  
+  def edit
+    @catalog = Catalog.find(params[:id])
+    @title = "Редактирование продукта"
+  end
+  
+  def update
+    @catalog = Catalog.find(params[:id])
+    if @catalog.update_attributes(params[:catalog])
+      redirect_to(@catalof, :notice => "Продукт успешно обновлен")
+    else
+      render(:action => "edit")
+    end
+  end
+  
+  def destroy
+    @catalog = Catalog.find(params[:id])
+    @catalog.destroy
+    redirect_to(catalogs_url)
+  end
 end
