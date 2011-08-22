d58bab30eb804ac9e55d52bbd6b32a369e08e3db
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-22 11:21:36 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kreslomeshok::Application.config.secret_token = 'eb9ee986a243c8e57cafd82b6bdd30e95d8f4aa42e7e4009f47e78687125f45e8b18778c2c71f7332765d3c76a6f064887f9d31abbeb578cc77e7e6875b3cd22'
