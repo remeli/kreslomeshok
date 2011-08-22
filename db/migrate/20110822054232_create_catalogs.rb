2d1d3b0980446c512ea08554c5a4ba40b6e70992
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-22 11:49:36 +0600

    edit migration for catalog

--- db/migrate/20110822054232_create_catalogs.rb
+++ db/migrate/20110822054232_create_catalogs.rb
@@ -1,12 +1,19 @@
 class CreateCatalogs < ActiveRecord::Migration
   def self.up
     create_table :catalogs do |t|
-
+      t.string :title, :null => false
+      t.string :permalink, :null => false
+      t.float :price
+      t.string :dimensions
+      t.string :photo_url
+      t.text :description
       t.timestamps
     end
+    add_index :catalogs, :title
   end
 
   def self.down
+    remove_index :catalogs, :column_name
     drop_table :catalogs
   end
 end
