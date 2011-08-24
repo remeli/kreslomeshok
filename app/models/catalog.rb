3cb53ca0ae73d25341c2c115017278236cb9b28c
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-24 14:01:48 +0600

    finish form

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,7 +2,7 @@ class Catalog < ActiveRecord::Base
   validates :title, :presence => { :message => "поле 'Название' обязательное"}
   validates :permalink, :presence => { :message => "поле 'Прямая ссылка' обязательное"}
   validates :price, :presence => { :message => "поле 'Цена' обязательное" }
-  
+  validates :dimensions, :presence => { :message => "поле 'Габариты' обязательное"}
   default_scope order("catalogs.id DESC")
   
   def to_param
