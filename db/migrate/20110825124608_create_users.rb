7effcbc3b211ff35e66e414346399bd342f90ead
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-25 19:05:12 +0600

    edit migrate

--- db/migrate/20110825124608_create_users.rb
+++ db/migrate/20110825124608_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
+      t.string :name
+      t.string :hashed_password
+      t.string :salt
       t.timestamps
     end
   end
