5ac3d1bd44545139529c3a21f1026b84563d0c97
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-25 11:07:38 +0600

    deleted news

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110825045829) do
+ActiveRecord::Schema.define(:version => 20110824110803) do
 
   create_table "catalogs", :force => true do |t|
     t.string   "title",              :null => false
@@ -27,12 +27,5 @@ ActiveRecord::Schema.define(:version => 20110825045829) do
   end
 
   add_index "catalogs", ["title"], :name => "index_catalogs_on_title"
-
-  create_table "news", :force => true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
