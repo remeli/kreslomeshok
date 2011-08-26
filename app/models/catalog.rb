62e980516e965424d7cfa48b57230e50cff8f213
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-26 22:46:44 +0600

    edit paperclip

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,13 +2,13 @@ class Catalog < ActiveRecord::Base
   
   default_scope order("catalogs.id DESC")
   
-  has_attached_file :photo, :styles => { :small => "150x150#", :medium => "300x300#" },
+  has_attached_file :photo, :styles => { :thumb => "150x150>", :medium => "300x300>" },
                             :url => "/system/:attachment/:id/:style/:basename.:extension",
           :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   
   validates_attachment_presence :photo, :message => "поле 'Фото' обязатенльное"
   validates_attachment_size :photo, :less_than => 5.megabytes
-
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
   
   validates :title, :presence => { :message => "поле 'Название' обязательное"}
   validates :permalink, :presence => { :message => "поле 'Прямая ссылка' обязательное"}
